0a3e29ed650c7165738e0dfcee61618b8d3377a9
ahmmuh <mukhtar1100@hotmail.com>
2022-01-02 21:03:33 +0100

    Laravel api

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+
+// Route::get('/', [PostController::class, 'index']);
+// Route::post('/posts', [PostController::class, 'store']);
+// Route::get('posts/{id}', [PostController::class, 'show']);
+// Route::put('posts/{id}', [PostController::class, 'update']);
+// Route::delete('posts/{id}', [PostController::class, 'destroy']);
